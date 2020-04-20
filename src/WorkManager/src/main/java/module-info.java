bab98d9be8baf19bcc23155f4801f36abea7a9de
danielandricic <danielandricic@hotmail.de>
2020-04-20 15:13:54 +0200

    Update GH-Page

--- src/WorkManager/src/main/java/module-info.java
+++ src/WorkManager/src/main/java/module-info.java
@@ -1,7 +1,7 @@
-module at.htl.workmanager {
+module main.java.at.htl.workmanager {
     requires javafx.controls;
     requires javafx.fxml;
 
     opens at.htl.workmanager.GUI to javafx.fxml;
-    exports at.htl.workmanager.GUI;
+    exports main.java.at.htl.workmanager.GUI;
 }
